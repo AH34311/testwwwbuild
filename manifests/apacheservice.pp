8ec5544f58dd2c07027519cd29e0918381e74004
Andrew Hall <andrew.hall@bristol.ac.uk>
2025-12-23 16:44:13 +0000

    creation of apache service module

---
+++ manifests/apacheservice.pp
@@ -0,0 +1,11 @@
+class testwwwbuild::apacheservice {
+  package { 'httpd':
+    ensure => installed,
+  }
+
+  service { 'httpd':
+    ensure  => running,
+    enable  => true,
+    require => Package['httpd'],
+  }
+}
